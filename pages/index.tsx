016bdae8e5a93894cd3151ef9358c8a4ce320f23
Manoel Duran <manoel.duran@hotmail.com>
2021-07-07 11:36:00 -0300

    resume finalizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
+import React from "react";
+import { VscGithubInverted } from 'react-icons/vsc';
+import { ImLinkedin } from 'react-icons/im';
+import styles from '../styles/styles.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <p>Hello! How you doing? I'm building my new portfolio.</p>
-      <p>Please, feel free to get in touch through any way you prefer:</p>
-      <p>You can contact me through my e-mail:<a href="">manoel.duran@hotmail.com</a></p>
-      <p>Or even on any social media platform:</p>
-      <div>
-        <a href="https://github.com/manoelduran">icon github</a>
-        <a href="https://www.linkedin.com/in/manoel-duran-63aaa0105/">icon linkedin</a>
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <p>Hello! How you doing? I'm building my new portfolio.</p>
+        <p>Please, feel free to get in touch through any way you prefer:</p>
+        <p>You can contact me through my e-mail:<a href="mailto:manoel.duran@hotmail.com">manoel.duran@hotmail.com</a></p>
+        <p>Or even on any social media platform:</p>
       </div>
-    </div>
+      <div className={styles.links}>
+        <a href="https://github.com/manoelduran">
+          <VscGithubInverted />
+        </a>
+        <a href="https://www.linkedin.com/in/manoel-duran-63aaa0105/">
+          <ImLinkedin />
+        </a>
+      </div >
+    </div >
   )
 }
