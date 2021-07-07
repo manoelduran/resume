d3a8c1e39195099765096d14949a2f3c0f182921
Manoel Duran <manoel.duran@hotmail.com>
2021-07-06 22:38:43 -0300

    faltando css

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <p>Hello! How you doing? I'm building my new portfolio.</p>
+      <p>Please, feel free to get in touch through any way you prefer:</p>
+      <p>You can contact me through my e-mail:<a href="">manoel.duran@hotmail.com</a></p>
+      <p>Or even on any social media platform:</p>
+      <div>
+        <a href="https://github.com/manoelduran">icon github</a>
+        <a href="https://www.linkedin.com/in/manoel-duran-63aaa0105/">icon linkedin</a>
+      </div>
+    </div>
+  )
+}
