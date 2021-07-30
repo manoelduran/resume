d94b7bee37293664d388e645d27d565063029507
Manoel Duran <manoel.duran@hotmail.com>
2021-07-30 09:35:12 -0300

    fixed linter problems

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.text}>
-        <p>Hello! How you doing? I'm building my new portfolio.</p>
+        <p>{"Hello! How you doing? I'm building my new portfolio."}</p>
         <p>Please, feel free to get in touch through any way you prefer:</p>
         <p>You can contact me through my e-mail:<a href="mailto:manoel.duran@hotmail.com">manoel.duran@hotmail.com</a></p>
         <p>Or even on any social media platform:</p>
