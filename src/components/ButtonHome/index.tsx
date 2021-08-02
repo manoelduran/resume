16cff61fb395b9d8c51d09d78fbb4bc71e73c32b
Manoel Duran <manoel.duran@hotmail.com>
2021-08-02 09:18:12 -0300

    link bug fixed

--- src/components/ButtonHome/index.tsx
+++ src/components/ButtonHome/index.tsx
@@ -1,10 +1,13 @@
 import styles from './styles.module.scss';
+import Link from 'next/link';
 export function ButtonHome() {
   return (
     <div className={styles.container}>
       <span>Desenvolvido por Manoel Duran</span>
       <button>
-        <a href="/">Voltar ao topo</a>
+        <Link href="/">
+          <a>Voltar ao topo</a>
+        </Link>
       </button>
     </div>
   );
