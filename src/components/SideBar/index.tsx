8cbbb242b4c27fdb60f1ea3b16e9b2621a933a68
Manoel Duran <manoel.duran@hotmail.com>
2021-08-02 09:15:05 -0300

    bug fixed

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,21 +1,28 @@
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
 import { ImLinkedin } from 'react-icons/im';
+import Image from 'next/image';
+import React from 'react';
+import Link from 'next/link';
 export function SideBar() {
   return (
     <div className={styles.container}>
       <section className={styles.contact}>
-        <img src="/manoelduran.jpeg" />
+        <Image src="/manoelduran.jpeg" width="150px" height="150px" />
         <h1>Manoel Duran</h1>
         <p>Software Developer</p>
       </section>
       <section className={styles.links}>
-        <a href="https://github.com/manoelduran">
-          <VscGithubInverted />
-        </a>
-        <a href="https://www.linkedin.com/in/manoel-duran-63aaa0105/">
-          <ImLinkedin />
-        </a>
+        <Link href="https://github.com/manoelduran">
+          <a>
+            <VscGithubInverted />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/manoel-duran-63aaa0105/">
+          <a>
+            <ImLinkedin />
+          </a>
+        </Link>
       </section>
     </div>
   );
