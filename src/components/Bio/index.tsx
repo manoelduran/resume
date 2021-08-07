4392c3abb18245705ce88f92baf9540e85c2ee4b
Manoel Duran <manoel.duran@hotmail.com>
2021-08-07 18:43:05 -0300

    bio changes

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -3,7 +3,7 @@ export function Bio() {
   return (
     <div className={styles.container}>
       <h1 >| Sobre Mim</h1>
-      <p>Ola, sou Manoel Duran e tenho 26 anos. Sou formado em Engenharia Civil a 2 anos e me tornei desenvolvedor por volta de 4 meses. Sempre gostei de ficar horas no computador por ser gamer, e desde a matéria introdutória de Algoritimos de programação, em que desenvolvi com C++, me interessei pela programação. Já trabalhei em uma empresa de desenvolvimento de Software, porém como gerente de projetos. Nela, tive a oportunidade de conhecer React.js, Next.js e React Native, e acabei me apaixonando pela área. Nas minhas horas de lazer, gosto de jogar Ragnarok Online, surfar ou estar com meus amigos.
+      <p>Ola, sou Manoel Duran e tenho 26 anos. Sou formado em Engenharia Civil há 2 anos e me tornei desenvolvedor por volta de 4 meses. Sempre gostei de ficar horas no computador por ser gamer, e desde a matéria introdutória de Algoritimos de programação, em que desenvolvi com C++, me interessei pela programação. Já trabalhei em uma empresa de desenvolvimento de Software, porém como gerente de projetos. Nela, tive a oportunidade de conhecer React.js, Next.js e React Native, e acabei me apaixonando pela área. Nas minhas horas de lazer, gosto de jogar Ragnarok Online, surfar ou estar com meus amigos.
       </p>
     </div>
   );
